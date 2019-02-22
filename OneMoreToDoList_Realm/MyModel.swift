c06940603bed22a7656b1db9481e74303897368c
Joshisaka1004 <joshi1004@gmail.com>
2019-02-22 11:34:15 +0100

    Realm with new Model file

---
+++ OneMoreToDoList_Realm/MyModel.swift
@@ -0,0 +1,17 @@
+//
+//  MyModel.swift
+//  OneMoreToDoList_Realm
+//
+//  Created by Joachim Vetter on 22.02.19.
+//  Copyright © 2019 Joachim Vetter. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class MyModel: Object {
+    @objc dynamic var myText: String = ""
+    @objc dynamic var myAge: Int = 0
+    @objc dynamic var myBool: Bool = false
+    @objc dynamic var arrayInCase: [String] = [String]()
+}
